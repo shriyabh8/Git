b3e03d72ab9e239bb70feb00f1a4e419f9ff4edd
shriya <shriya.dharmendran@gmail/com>
2023-06-12 19:42:43 -0400

    Question 1 of Movie Quiz

--- Git/MovieQuiz.swift
+++ Git/MovieQuiz.swift
@@ -8,10 +8,76 @@
 import SwiftUI
 
 struct MovieQuiz: View {
+    @State private var result = " "
     var body: some View {
-        Text("Movie Quiz")
+        ZStack {
+            Color(.white)
+                .ignoresSafeArea()
+        
+            VStack {
+               
+                
+                Text("What is your favorite Genre?")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Image("Movie Chairs")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                
+                Button("Action"){
+                    result = "Action"
+                }
+                .font(.title2)
+                .tint(.red)
+                .buttonStyle(.borderedProminent)
+                
+                Button("Romance"){
+                    result = "Romance"
+                }
+                .font(.title2)
+                .tint(.indigo)
+                .buttonStyle(.borderedProminent)
+                
+                Button("Comedy"){
+                    result = "Comedy"
+                }
+                .font(.title2)
+                .tint(.yellow)
+                .buttonStyle(.borderedProminent)
+                
+                Button("Drama"){
+                    result = "Drama"
+                }
+                .font(.title2)
+                .tint(.red)
+                .buttonStyle(.borderedProminent)
+                
+                Button("Horror"){
+                    result = "Horror"
+                }
+                .font(.title2)
+                .tint(.indigo)
+                .buttonStyle(.borderedProminent)
+                
+                NavigationLink(destination: movieQuizQ2()){
+                    Text("Next")
+                    
+                }
+                
+                .font(.title2)
+                .tint(.yellow)
+                .buttonStyle(.borderedProminent)
+                
+            }
+            
+            
+        }
+    
+        
     }
 }
+
 
 struct MovieQuiz_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Git/movieQuizQ2.swift
@@ -0,0 +1,20 @@
+//
+//  movieQuizQ2.swift
+//  Git
+//
+//  Created by Shriya Dharmendran on 6/12/23.
+//
+
+import SwiftUI
+
+struct movieQuizQ2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct movieQuizQ2_Previews: PreviewProvider {
+    static var previews: some View {
+        movieQuizQ2()
+    }
+}
