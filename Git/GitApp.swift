77d9495ec854520cec09014f7a28288401bc6a1c
Shriya Bhamidipati <shriya.bhamidipati@gmail.com>
2023-06-12 09:07:09 -0700

    Initial Commit

---
+++ Git/GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitApp.swift
+//  Git
+//
+//  Created by Shriya Bhamidipati on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
